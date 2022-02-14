064156cc8d7e422aa6783e2c23e1876083d9febf
Zioło <ziolko@student.agh.edu.pl>
2022-02-14 20:10:04 +0100

    All Test

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,30 +1,89 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Collection;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
     private Collection<Product> products;
+    private BigDecimal netValue;
+    private BigDecimal total;
+    private BigDecimal tax;
 
-    public void addProduct(Product product) {
-        // TODO: implement
+    // konstruktor
+    Invoice () {
+        netValue = new BigDecimal ("0");
+        total = new BigDecimal ("0");
+        tax = new BigDecimal ("0");
+        products = new ArrayList<Product> ();
+
     }
 
-    public void addProduct(Product product, Integer quantity) {
-        // TODO: implement
+    public void addProduct (Product product) {
+        products.add (product);
+        update ();
     }
 
-    public BigDecimal getSubtotal() {
-        return null;
+    public void addProduct (Product product, Integer quantity) {
+        if (quantity <= 0) {
+            throw new IllegalArgumentException ();
+        }
+        for (int i = 0; i < quantity; i++) {
+            products.add (product);
+        }
+        update ();
     }
 
-    public BigDecimal getTax() {
-        return null;
+
+    public void setNetValue () {
+        netValue = BigDecimal.ZERO;
+        for (Product product : products) {
+            netValue = netValue.add (product.getPrice ());
+        }
     }
 
-    public BigDecimal getTotal() {
-        return null;
+
+    public BigDecimal getNetValue () {
+        return netValue;
+    }
+
+    public void setTax () {
+        tax = total.subtract (netValue);
+    }
+
+    public BigDecimal getTax () {
+        return tax;
+    }
+
+    public void setTotal () {
+        total = BigDecimal.ZERO;
+        for (Product product : products) {
+            total = total.add (product.getPriceWithTax ());
+        }
+    }
+
+    public BigDecimal getTotal () {
+        return total;
+    }
+
+    public static Invoice create () {
+        return new Invoice ();
+    }
+
+    public void update () {
+        setNetValue ();
+        setTotal ();
+        setTax ();
+    }
+
+    public BigDecimal getSubtotal () {
+        BigDecimal sum = BigDecimal.ZERO;
+        for (Product product : products) {
+            sum = sum.add (product.getPrice ());
+        }
+        return sum;
     }
 }
